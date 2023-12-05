8d2ac3fc9fd67312b19902d98c80f39879d7fbc5
Alyx BB <git@alyxbb.tech>
2023-12-05 03:16:57 +0000

    day 4

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -1,13 +1,49 @@
+use std::array;
+
 use crate::{Solution, SolutionPair};
 
 fn part_1(input: String) -> Solution {
-    let sol = 0;
-    Solution::from(sol)
+    let mut tot = 0;
+    for mut line in input.lines(){
+        let mut card_score = 0;
+        line = line.split(": ").last().expect("card number is missing");
+        let subparts = line.split(" | ").collect::<Vec<&str>>();
+        let winners: Vec<&str> = subparts[0].split_whitespace().collect();
+        let mine = subparts[1];
+        for num in mine.split_whitespace() {
+            if winners.contains(&num) {
+                if card_score == 0 {
+                    card_score = 1;
+                } else {
+                    card_score *= 2;
+                }
+            }
+        }
+        tot += card_score;
+    }
+    Solution::from(tot)
 }
 
 fn part_2(input: String) -> Solution {
-    let sol = 0;
-    Solution::from(sol)
+    let mut card_counts = [0; 200];
+    for (i,mut line) in input.lines().enumerate(){
+        card_counts[i] +=1;
+        let card_count = card_counts[i];
+        let mut card_score = 0;
+        line = line.split(": ").last().expect("card number is missing");
+        let subparts = line.split(" | ").collect::<Vec<&str>>();
+        let winners: Vec<&str> = subparts[0].split_whitespace().collect();
+        let mine = subparts[1];
+        for num in mine.split_whitespace() {
+            if winners.contains(&num) {
+                card_score += 1;
+            }
+        }
+        for j in 0..card_score  {
+            card_counts[i+j+1]+=card_count;
+        }
+    }
+    Solution::from(card_counts.iter().sum::<u32>())
 }
 ///////////////////////////////////////////////////////////////////////////////
 
